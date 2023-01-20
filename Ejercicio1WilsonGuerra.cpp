0c9dfc330625da44385262f3094a4b39d3ff8b40
ExCalibur6897 <75451548+ExCalibur6897@users.noreply.github.com>
2023-01-20 15:13:21 -0600

    Add files via upload

---
+++ Ejercicio1WilsonGuerra.cpp
@@ -0,0 +1,35 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//Calcular el error numerico
+	cout << "Error numerico" << endl;\
+		cout << "Ingrese N:" << endl;
+	double N;
+	cin >> N;
+	float x = 1 / N;
+	double x1 = 1 / N;
+	long double x2 = 1 / N;
+	float s = 0;
+	double s1 = 0;
+	long double s2 = 0;
+	for (int i = 0; i < N; i++)
+	{
+		s = s + x;
+		s1 = s1 + x1;
+		s2 = s2 + x2;
+	}
+	//Calcular el error 𝐸 = |1.0 − 𝑆| 
+	float E = 1.0 - s;
+	double E1 = 1.0 - s1;
+	long double E2 = 1.0 - s2;
+	cout << "El error usando float es: " << E << endl;
+	cout << "El error usando double es: " << E1 << endl;
+	cout << "El error usando long double es: " << E2 << endl;
+	return 0;
+}
+
---
+++ Ejercicio2WilsonGuerra.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//Division de dos entradas
+	cout << "Ingrese P: " << endl;
+	int p;
+	cin >> p;
+	cout << "Ingrese A: " << endl;
+	int a;
+	cin >> a;
+	cout << "Ingrese B: " << endl;
+	int b;
+	cin >> b;
+	int M = a/b;
+	
+	//Calculo de M
+	for (int i = 0; i < p-1; i++)
+	{
+			M += a / (b - 1);
+	}
+	
+	//Salida de M
+	cout << "M = " << M << endl;
+	return 0;
+}
+
